b4d171291a97857eda4cafcf94231bde04407eff
tuliofh01 <tuliofh01@gmail.com>
2023-07-13 14:26:24 -0300

    css styling

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -5,16 +5,37 @@ function Index(){
 
     const navigate = useNavigate();
 
-    return(
-        <div className={styles.container + " " + styles.background}>
-            <h1 className={styles.title}>juegos</h1>
-            <h3 className={styles.h3}>Available titles:</h3>
-            <ul className={styles.ul}>
-                <li className={styles.li} onClick={() => {navigate("/minefield")}}>Minefield</li>
-                <li className={styles.li} onClick={() => {navigate("/guessTheNumber")}}>Guess the Number</li>
-                <li className={styles.li} onClick={() => {navigate("/crosswordPuzzle")}}>Crossword Puzzle</li>
-            </ul>
-        </div>
+    return (
+      <div className={styles.container + " " + styles.background}>
+        <h1 className={styles.title}>juegos</h1>
+        <ul className={styles.ul + " " + styles.liquid}>
+          <h3 className={styles.h3}>Available titles:</h3>
+          <li
+            className={styles.li}
+            onClick={() => {
+              navigate("/minefield");
+            }}
+          >
+            Minefield
+          </li>
+          <li
+            className={styles.li}
+            onClick={() => {
+              navigate("/guessTheNumber");
+            }}
+          >
+            Guess the Number
+          </li>
+          <li
+            className={styles.li}
+            onClick={() => {
+              navigate("/crosswordPuzzle");
+            }}
+          >
+            Crossword Puzzle
+          </li>
+        </ul>
+      </div>
     );
 }
 
